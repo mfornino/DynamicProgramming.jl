3bacd759202c02e7c4c52006b8e6b4f38904e7fe
Michele Fornino <fornino.michele@gmail.com>
2020-09-29 15:08:36 -0400

    made sure package is in working condition. added some codes to test folder for future implementation

--- src/DynamicProgramming.jl
+++ src/DynamicProgramming.jl
@@ -1,5 +1,30 @@
 module DynamicProgramming
 
-# Write your package code here.
+# Dependencies
+using LinearAlgebra
+using SparseArrays
+using MarkovChains
+using ProgressMeter
+
+import Base: 
+	show
+
+
+# Include statements
+include("hjb.jl")
+
+
+# Export types
+export 
+	HJBEquation
+
+# Export methods
+export 
+	solve
+
+"""
+A Julia package for solving Dynamic Programming problems.
+
+"""
 
 end
